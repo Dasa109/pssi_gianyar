1a1ac0cfedd2aefe614c7be6931d8df0cc8e8d74
I WAYAN PASEK GEDE SEGARA CAYMAN <iwayanpasek23@gmail.com>
2026-02-17 17:56:53 +0800

    update 7.1
    fix lokasi club

--- resources/views/pages/clubs/index.blade.php
+++ resources/views/pages/clubs/index.blade.php
@@ -45,7 +45,10 @@
                         
                         <div class="mt-4 flex items-center gap-2 text-zinc-500 text-xs font-semibold uppercase tracking-wide">
                             <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg>
-                            {{ $club->stadium_name ?? 'Gianyar' }}
+                            
+                            {{-- PERBAIKAN DI SINI: Ganti stadium_name jadi stadium --}}
+                            {{ $club->stadium ?? 'Stadion Belum Diisi' }}
+                            
                         </div>
                     </div>
                 </a>
