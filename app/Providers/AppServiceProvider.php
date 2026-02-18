d55ec936050ae8b5e9c38a19c69786468b015fb1
I WAYAN PASEK GEDE SEGARA CAYMAN <iwayanpasek23@gmail.com>
2026-02-18 08:25:39 +0800

    Revert "Update AppServiceProvider.php"
    This reverts commit f23e8d57813c0019927ef893580d1cbd368291c2.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        // PENTING: Paksa HTTPS jika di production/cloud
-        if($this->app->environment('production') || $this->app->environment('local')) { // Sesuaikan environment
-            URL::forceScheme('https');
-        }
+        //
     }
 }
